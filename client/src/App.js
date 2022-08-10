7d93e894d6650ef7fa4bc4209d63d4cbff2b3a22
Lucas Mace <lucasmace4130@gmail.com>
2022-08-09 23:56:43 -0500

    Adding fun console log

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,64 @@ import "./global.css";
 import ScrollToTop from './Components/ScrollToTop';
 
 function App() {
+  console.log("%cI am looking for work!", `
+    display: block;
+    font-family: system-ui;
+    padding: 50px;
+    color: white;
+    text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+    font-size: 36px;
+    font-weight: bold;
+    text-align: center; 
+    width: 100%;
+    background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(255,0,0,1) 100%);
+    border-radius: 36px;
+    `);
+  console.log("%cPlease send an email to:", `
+    display: block;
+    font-family: system-ui;
+    padding: 10px;
+    color: white;
+    font-size: 14px;
+    font-weight: bold;
+    text-align: center; 
+    width: 100%;
+    background-color: black;
+    `);
+  console.log("%clucasmace4130@gmail.com", `
+    display: block;
+    font-family: system-ui;
+    padding: 10px;
+    color: blue;
+    text-decoration: underline;
+    font-size: 20px;
+    font-weight: bold;
+    text-align: center; 
+    width: 100%;
+    background-color: white; 
+    `);
+  console.log("%cI look forward to hearing from you.", `
+    display: block;
+    font-family: system-ui;
+    padding: 14px;
+    color: white;
+    font-size: 12px;
+    font-weight: bold;
+    text-align: center; 
+    width: 100%;
+    background-color: black;
+    `);
+  console.log("%cThank you for visiting.", `
+    display: block;
+    font-family: system-ui;
+    padding: 14px;
+    color: white;
+    font-size: 12px;
+    font-weight: bold;
+    text-align: center; 
+    width: 100%;
+    background-color: black;
+    `);
 
   return (
     <Router>
