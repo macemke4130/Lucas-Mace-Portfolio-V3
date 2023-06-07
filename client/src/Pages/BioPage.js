90db17c97e26a29c416aafde41ed27e3c71fa679
Lucas <lucasmace4130@gmail.com>
2023-06-07 09:52:20 -0400

    Updating links - lM

--- client/src/Pages/BioPage.js
+++ client/src/Pages/BioPage.js
@@ -18,7 +18,7 @@ function BioPage() {
                         <p>I currently work as the Lead Front End Web, React, UX/UI and VTEX Developer for <a href="https://www.eriksbikeshop.com/" rel="noreferrer" target="_blank">Erik's Bike Shop</a>, one of the largest bicycle and Winter sports shops in America.</p>
                         <p>My favorite tech stack currently is React, GraphQL, Express and MySQL. If you're a bicycle nerd, you may enjoy my <a href="http://www.namethatpart.com/" rel="noreferrer" target="_blank">Name That Part</a> game that utilizes those technologies.</p>
                         <p>In my free time, I enjoy fixing bicycles, but they're much more fun to ride. I also love coffee, hiking, camping, skateboarding, photography, live music and reading science fiction.</p>
-                        <p>Please check out my <Link href="/resume">resume</Link> and my <Link href="/skills">skills</Link> list for more information.</p>
+                        <p>Please check out my <Link to="/resume">resume</Link> and my <Link to="/skills">skills</Link> list for more information.</p>
                     </div>
                 </div>
             </div>
